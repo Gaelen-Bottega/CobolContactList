ea9294b890c0b32f326b5682e3449ea930ff0923
Gaelen Rhoads <gaelenrhoads@gmail.com>
2022-01-19 23:08:54 -0500

    Added variables for contact and opened connection to out file

--- A1-ContactList/A1-ContactList.cbl
+++ A1-ContactList/A1-ContactList.cbl
@@ -9,7 +9,7 @@
       *
        environment division.
        configuration section.
-      * Deliberately blank
+      *
        input-output section.
        file-control.
       *    
@@ -23,40 +23,58 @@
       * Declare how long the output lines will be in our file
        fd output-file
            data record is output-line
-           record contains 44 characters.
-      *
-       01 output-line              pic X(44).
+           record contains 62 characters.
+      * Declare variable to write to for out file
+       01 output-line              pic X(62).
       *
        working-storage section.
+      * Declare heading for file with space (filler) on each side
        01 ws-heading.
-         05 filler                 pic X(12)
-                           value spaces.
-         05 ws-heading-title       pic X(22)
-                           value "MAFD 4202 Contact List".
-         05 filler                 pic X(12)
-                           value spaces.
-      *
-       01 ws-accept-message pic X(45).
-       
+         05 filler                     pic X(20)
+                   value spaces.
+         05 ws-heading-title           pic X(22)
+                   value "MAFD 4202 Contact List".
+         05 filler                     pic X(20)
+                   value spaces.
+      * Declare contact details
+       01 ws-contact.
+         05 ws-name                    pic X(20).
+         05 ws-email                   pic X(30).
+         05 ws-phone-number            pic X(12).
+      * Declare message for console window
+       01 ws-accept-message            pic X(50).
       *
        procedure division.
        000-main.
-      *    move "Gaelen" to ws-first.
-      *    move "Rhoads" to ws-family.
-      *    move "Hello"  to ws-message.
-      *    display ws-message ws-first ws-family.
-      *
+      * Opening the connection to output file and writing to it
            open output output-file.
-           move ws-name to output-line.
-           write output-line.
-           close output-file.
-      *
+      * Display header and add spaces in between records
+           write output-line from ws-heading after advancing 1 line.
+      * Assign values to previously declared variables
+           move "Gaelen Rhoads"                to ws-name.
+           move "gaelen.rhoads@dcmail.ca"      to ws-email.
+           move "203-393-5200"                 to ws-phone-number.
+      * Write the contact to the file and add space
+           write output-line from ws-contact after advancing 2 lines.
+      * Reassign variables for next contact
+           move "Marcus Chadwick"              to ws-name.
+           move "marcus.chadwick@dcmail.ca"    to ws-email.
+           move "203-412-3748"                 to ws-phone-number.
+      * Write new contact to file with space
+           write output-line from ws-contact.
+      * Reassign information for last contact
+           move "John Smith"                   to ws-name.
+           move "john.smith@dcmail.ca"         to ws-email.
+           move "203-393-5200"                 to ws-phone-number.
+      * Write last contact to file
+           write output-line from ws-contact.
+      * Assign console output to variable
            move "Press the enter key to exit the program..."
-             to ws-accept-message.
-      *
+                                               to ws-accept-message.
+      * Display instructions to user to close console window
            display ws-accept-message.
            accept ws-accept-message.
-      * Close output file
+      * Close output file and end program
            close output-file
            goback.
        end program A1-ContactList.
